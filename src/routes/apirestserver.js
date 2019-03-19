6c00f80f694207472f7f70dbd12aa6828c0871bd
Willian Rodelo <36023500+willianrodelogarcia@users.noreply.github.com>
2019-03-19 17:19:16 -0500

    Update apirestserver.js

--- src/routes/apirestserver.js
+++ src/routes/apirestserver.js
@@ -4,86 +4,102 @@ var Request = require('tedious').Request;
 
 const router = express.Router();
 
-
 const mssql = require('../database');
 
 
-router.get('/api/ambientes',(req,res)=>{
-    var serviceData = []
-    var request = new Request("select * from ambientes",(err,rowCount,rows)=>{
-        if(err){
-            res.json({"ErrorServices": err})
+
+
+router.get('/', (req, res) => {
+
+    var request = new Request("SELECT * FROM ambientes", (err, rowCount, rows) => {
+        if (!err) {
+            //res.json(rows[0][0].value);
+            //res.send('Funciona Bien'); 
+        } else {
+            res.json(err);
+
         }
+
+        //res.json(rows)
+
     });
-
-    request.on('row',(columns)=>{
-
+    var datas = []
+    var i = 0;
+    request.on('row', function (columns) {
         var item = {}
-
-        columns.forEach(column => {
+         columns.forEach(column => {
+            //console.log("%s\t%s", column.metadata.colName, column.value);
             item[column.metadata.colName] = column.value;
-        });
+         });
+         datas.push(item)
 
-        serviceData.push(item)
     });
 
-    request.on('done',()=>{
-        res.status(200).json(serviceData);
+    request.on('done', function (rowCount, more, rows) { 
+        console.log(rowCount)
+        res.status(502).json(datas)
     });
-
 
     mssql.execSqlBatch(request);
 
-});
 
-router.get('/api/huespeds',(req,res)=>{
-
-    var huespedData = [];
-    var request = new Request("select * from huesped",(err,rowCount,rows)=>{
-        if(err){
-            res.json({"HuespedErr":err})
-        }
-    });
-
-    request.on('row',(columns)=>{
-        var item = {}
-        columns.forEach(column => {
-            item[column.metadata.colName] = column.value;
-        });
-
-        huespedData.push(item)
-    });
-
-    request.on('done',()=>{
-        res.status(200).json(huespedData)
-    });
-})
-
-
-router.post('/login',(req,res)=>{
-    var { identificacion } = req.body;
-    var huespedData = [];
-    var request = new Request("select * from huesped where IdentificacionHusped ="+identificacion,(err,rowCount,rows)=>{
-        if(err){
-            res.json({"HuespedErr":err})
-        }
-    });
-
-    request.on('row',(columns)=>{
-        var item = {}
-        columns.forEach(column => {
-            item[column.metadata.colName] = column.value;
-        });
-
-        huespedData.push(item)
-    });
-
-    request.on('done',()=>{
-        res.status(200).json(huespedData)
-    });
-    
 });
 
 
+/*router.get('/:id', (req, res) => {
+    var { id } = req.params;
+    //console.log(id);
+    mssql.query('SELECT * FROM ambientes WHERE AmbienteID =' + id, (err, rows, fields) => {
+        if (!err) {
+            if (rows.recordset.length !== 0) {
+                res.json(rows.recordset);
+                console.log("Rows", rows.recordset.length)
+            } else {
+                res.json({
+                    "Status": "Dato no Existe"
+                });
+            }
+        } else {
+            console.log(err);
+        }
+    });
+});
+router.post('/add', (req, res) => {
+    const { NombreAmmbiente } = req.body;
+    mssql.query("INSERT INTO ambientes (NombreAmmbiente) VALUES ('" + NombreAmmbiente + "')", (err, rows, fields) => {
+        if (!err) {
+            res.json({ 'status': 'Dato Agregado' });
+        } else {
+            res.json({
+                "Error": err
+            });
+        }
+    });
+});
+router.put('/edit/:id', (req, res) => {
+    const { NombreAmmbiente } = req.body;
+    const { id } = req.params;
+    mssql.query("UPDATE ambientes SET NombreAmmbiente ='" + NombreAmmbiente + "' WHERE AmbienteID = " + id, (err, rows, fields) => {
+        if (!err) {
+            res.json({ 'status': 'Dato Editado' });
+        } else {
+            res.json({
+                "Error": err
+            });
+        }
+    });
+});
+router.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    mssql.query('DELETE FROM ambientes WHERE AmbienteID =' + id, (err, rows, fields) => {
+        if (!err) {
+            res.json({ 'status': 'Dato Eliminado' });
+        } else {
+            res.json({
+                "Error": err
+            });
+        }
+    });
+});*/
 
 module.exports = router;
